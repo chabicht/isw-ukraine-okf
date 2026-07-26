8207243cfc6372021018434a59d65c5a1309ccb2
Carsten Habicht <ch@chabicht.com>
2026-07-26 03:20:26 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 FILE="$1"
-PROMPT="$(echo -e "please read okf.md and rules.md and incorporate the file $FILE\nyou're running in auto mode, so don't ask questions and complete the task autonomously.\nfeel free to spawn subagents as suitable.\nif you're done and the validation passes, commit the update under the date yyyy-mm-dd and with a short list of changes.")"
+PROMPT="$(echo -e "please read okf.md and rules.md and incorporate the file $FILE\nyou're running in auto mode, so don't ask questions and complete the task autonomously.\nfeel free to spawn subagents as suitable.\nif you're done and the validation passes, commit the source file and updated okf under the date yyyy-mm-dd and with a short list of changes.\ndon't add unrelated source files.")"
 
 echo "Processing: $FILE"
 git add "$FILE"
